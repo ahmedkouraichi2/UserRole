3ec1ac357fb5517aca78e6cb027993bab6db884c
Your Name <kouraichiahmed2@gmail.com>
2022-11-29 18:11:54 +0100

    ajoute de UserServiceImp

--- src/main/java/com/enotes/Services/UserServiceImp.java
+++ src/main/java/com/enotes/Services/UserServiceImp.java
@@ -1,5 +1,8 @@
 package com.enotes.Services;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,33 +12,36 @@ import com.enotes.entity.User;
 import com.enotes.repository.RoleRepository;
 import com.enotes.repository.UserRepository;
 
+import lombok.AllArgsConstructor;
+
 
 @Service
 @Transactional 
+
+@AllArgsConstructor 
 public class UserServiceImp implements  UserServices{
 	
-	@Autowired 
+
 	private UserRepository userRepository;
 	
-	@Autowired 
+
 	private RoleRepository roleRepository ;
 
 	@Override
 	public User addUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		user.setUserId(UUID.randomUUID().toString());
+		return  userRepository.save(user);
 	}
 
 	@Override
 	public Role addRole(Role role) {
-		// TODO Auto-generated method stub
-		return null;
+		 return roleRepository.save(role);
 	}
 
 	@Override
 	public User finUserByUserName(String userName) {
-		// TODO Auto-generated method stub
-		return null;
+      return null;
 	}
 
 	@Override
--- src/main/java/com/enotes/repository/RoleRepository.java
+++ src/main/java/com/enotes/repository/RoleRepository.java
@@ -1,5 +1,13 @@
 package com.enotes.repository;
 
-public interface RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.enotes.entity.Role;
+
+
+@Repository
+public interface RoleRepository  extends JpaRepository<Role,Long>{
+	 Role findByRoleName(String roleName );
 
 }
